5f7acd90cd45bcc8dc29f196825401ab68ee8b9f
Babacar Ndong <ndongbabacar100@gmail.com>
2021-02-14 18:06:41 +0000

    ajout de mise en page 1

--- index.php
+++ index.php
@@ -6,26 +6,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Débuter avec git & gitHUB!!">
     <title>⪢ Apprendre Git/GitHUB : Les bases de la versionning avec Git/GitHUB - MaxLook</title>
+
+    <style>
+    body {
+        margin: 30px;
+        padding: 30px;
+    }
+
+    h1 {
+        color: orange;
+    }
+
+    .p_lesson {
+        padding: 10px;
+        background: #EAEAEA;
+    }
+    </style>
 </head>
 
 <body>
     <h1>Git</h1>
     <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus, atque.</h3>
+    <br>
+    <div class="p_lesson">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates, ducimus consequuntur atque repudiandae
+            voluptatum debitis explicabo minus vitae impedit rerum, molestias, illo nihil totam! Vel nostrum, debitis
+            mollitia deleniti aperiam blanditiis cum incidunt delectus velit. Commodi error voluptatum ducimus similique
+            sequi odit voluptas in, inventore culpa voluptates, dicta impedit magni, fugit tenetur veniam necessitatibus
+            eum
+            nihil ab accusamus eius at.</p>
+        <h3>Lorem ipsum dolor sit amet.</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic debitis iure a dolor rem itaque suscipit ad
+            harum
+            modi maxime, libero officia eligendi voluptate at vero fugiat cumque. Possimus, repudiandae?</p>
+    </div>
     <br><br>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates, ducimus consequuntur atque repudiandae
-        voluptatum debitis explicabo minus vitae impedit rerum, molestias, illo nihil totam! Vel nostrum, debitis
-        mollitia deleniti aperiam blanditiis cum incidunt delectus velit. Commodi error voluptatum ducimus similique
-        sequi odit voluptas in, inventore culpa voluptates, dicta impedit magni, fugit tenetur veniam necessitatibus eum
-        nihil ab accusamus eius at.</p>
-    <h3>Lorem ipsum dolor sit amet.</h3>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic debitis iure a dolor rem itaque suscipit ad harum
-        modi maxime, libero officia eligendi voluptate at vero fugiat cumque. Possimus, repudiandae?</p>
-
     <h1>GithHUB</h1>
     <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, id quae. Omnis.</h3>
-    <br><br>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse recusandae perferendis quidem. Ea nemo veritatis,
-        unde tenetur provident nobis, hic dignissimos ullam debitis, accusamus voluptas.</p>
+    <br>
+    <div class="p_lesson">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse recusandae perferendis quidem. Ea nemo
+            veritatis,
+            unde tenetur provident nobis, hic dignissimos ullam debitis, accusamus voluptas.</p>
+    </div>
     <ul>
         <li>Lorem ipsum dolor sit.</li>
         <li>Lorem ipsum dolor sit.</li>
@@ -39,9 +62,13 @@
     </ul>
 
     <h4>Conclusion</h4>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, nesciunt eum voluptatum quas, reprehenderit
-        neque corporis suscipit iure accusantium minima doloribus quisquam aperiam beatae dicta, unde deleniti magnam
-        voluptatibus autem.</p>
+    <div class="p_lesson">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, nesciunt eum voluptatum quas,
+            reprehenderit
+            neque corporis suscipit iure accusantium minima doloribus quisquam aperiam beatae dicta, unde deleniti
+            magnam
+            voluptatibus autem.</p>
+    </div>
 </body>
 
 </html>
