86f9679dd90fc2545957d63ac819247ecc060d74
Babacar Ndong <ndongbabacar100@gmail.com>
2021-02-14 17:43:10 +0000

    Début apprentissage Git collaboration avec GitHUB

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Débuter avec git & gitHUB!!">
+    <title>⪢ Apprendre Git/GitHUB : Les bases de la versionning avec Git/GitHUB - MaxLook</title>
+</head>
+
+<body>
+    <h1>Git</h1>
+    <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus, atque.</h3>
+    <br><br>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates, ducimus consequuntur atque repudiandae
+        voluptatum debitis explicabo minus vitae impedit rerum, molestias, illo nihil totam! Vel nostrum, debitis
+        mollitia deleniti aperiam blanditiis cum incidunt delectus velit. Commodi error voluptatum ducimus similique
+        sequi odit voluptas in, inventore culpa voluptates, dicta impedit magni, fugit tenetur veniam necessitatibus eum
+        nihil ab accusamus eius at.</p>
+    <h3>Lorem ipsum dolor sit amet.</h3>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic debitis iure a dolor rem itaque suscipit ad harum
+        modi maxime, libero officia eligendi voluptate at vero fugiat cumque. Possimus, repudiandae?</p>
+
+    <h1>GithHUB</h1>
+    <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, id quae. Omnis.</h3>
+    <br><br>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse recusandae perferendis quidem. Ea nemo veritatis,
+        unde tenetur provident nobis, hic dignissimos ullam debitis, accusamus voluptas.</p>
+    <ul>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+    </ul>
+
+    <h4>Conclusion</h4>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, nesciunt eum voluptatum quas, reprehenderit
+        neque corporis suscipit iure accusantium minima doloribus quisquam aperiam beatae dicta, unde deleniti magnam
+        voluptatibus autem.</p>
+</body>
+
+</html>
